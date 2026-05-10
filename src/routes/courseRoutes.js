3aa9864e74b7b375105a97e5b6d7bd919605e5f3
robiulalam02 <robiulbusiness02@gmail.com>
2026-05-10 21:34:41 +0600

    fixed delete course api issue

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -13,16 +13,16 @@ const upload = require('../middlewares/uploadMiddleware');
 
 // Public route to get courses
 router.get("/", getCourses);
-
 // Get courses by categories for education page
 router.get("/education", getEducationPageData);
-
 // Dashboard Route (MUST BE ABOVE /:id)
 router.get("/teacher/my-courses", protect, instructor, getTeacherCourses);
 
 // CRUD Routes for Instructors
 router.post("/teacher/add-course", protect, instructor, upload.single('image'), createCourse);
 router.put("/teacher/:id", protect, instructor, upload.single('image'), updateCourse);
-router.delete("/:id", protect, instructor, deleteCourse);
+
+// DELETE Course
+router.delete("/teacher/delete-course/:id", protect, instructor, deleteCourse);
 
 module.exports = router;
